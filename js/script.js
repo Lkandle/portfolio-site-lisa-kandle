fdea3ee12f2ac3ba9c69a29286bcea16712c6010
Lkandle <Lfinley9416@gmail.com>
2024-09-02 11:31:33 -0700

    add a mobile menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Hi! Welcome to my portfolio site. I'm glad your here!");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    x.className += ' responsive';
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
